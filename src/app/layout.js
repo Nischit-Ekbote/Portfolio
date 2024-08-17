08e54a18d5efc456c3a18c91d2823cd3b4280159
nischit <nischitekbote18@gmail.com>
2024-08-17 07:44:22 +0530

    2nd

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar/NavBar";
+import Footer from "@/components/Footer/Footer";
 import NamasteWrapper from "@/components/namaste/NamasteWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {<NamasteWrapper><NavBar />{children}</NamasteWrapper>}
+        {<NamasteWrapper><NavBar />{children}<Footer/></NamasteWrapper>}
       </body>
     </html>
   );
